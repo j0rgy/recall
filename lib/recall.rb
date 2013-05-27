9060c06364ae4172aa5d0842ced305dde4497cae
j0rgy <johnjorg@gmail.com>
2013-05-27 16:32:45 -0700

    Added note 'post' functionality & route'

--- lib/recall.rb
+++ lib/recall.rb
@@ -1,13 +1,13 @@
-require "recall/version"
+require_relative "recall/version"
 require "sinatra"
-require "datamapper"
+require "data_mapper"
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
 
 class Note
 	include DataMapper::Resource
 	property :id, Serial # id field which will be an integer primary key and auto-incrementing
-	property :content, Text :required => true
+	property :content, Text, :required => true
 	property :complete, Boolean, :required => true, :default => false
 	property :created_at, DateTime
 	property :updated_at, DateTime
@@ -22,4 +22,14 @@ get '/' do
 	@title = 'All Notes'
 	erb :home
 end
+
+post '/' do
+	n = Note.new # create a new Note object in the notes table in the database
+	n.content = params[:content] # The content field is set to the submitted data from the text area
+	n.created_at = Time.now # current timestamp
+	n.updated_at = Time.now # current timestamp
+	n.save
+	redirect '/'
+end
+
 
